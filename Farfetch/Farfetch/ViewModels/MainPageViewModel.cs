0936181245da5693cfae18911ad6331734a2a0ad
William Bello <willjobs94@gmail.com>
2016-10-05 06:03:15 -0400

    Remove unused references and comments

--- Farfetch/Farfetch/ViewModels/MainPageViewModel.cs
+++ Farfetch/Farfetch/ViewModels/MainPageViewModel.cs
@@ -1,35 +1,19 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Farfetch.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
-    {
-        private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
 
-        public MainPageViewModel()
-        {
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
 
-        }
+		}
 
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
 
-        }
-
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
-        }
-    }
+		}
+	}
 }
