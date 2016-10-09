d700d7f8bad7a3508a616efbcbbc365ec568d190
William Bello <willjobs94@gmail.com>
2016-10-08 23:09:10 -0400

    Change TabbedPage bar style

--- Farfetch/Farfetch.iOS/AppDelegate.cs
+++ Farfetch/Farfetch.iOS/AppDelegate.cs
@@ -24,7 +24,8 @@ namespace Farfetch.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+			UITabBar.Appearance.TintColor  = UIColor.Black;
+			global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App(new iOSInitializer()));
 
             return base.FinishedLaunching(app, options);
