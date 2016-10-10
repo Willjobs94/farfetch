01d2a37934db6a824f127e945b1aa046e41b2a50
William Bello <willjobs94@gmail.com>
2016-10-10 01:59:59 -0400

    Add boutique detail page and view model

--- Farfetch/Farfetch/ViewModels/BoutiqueDetailPageViewModel.cs
+++ Farfetch/Farfetch/ViewModels/BoutiqueDetailPageViewModel.cs
@@ -13,13 +13,29 @@ namespace Farfetch.ViewModels
 		{
 			_navigationService = navigationService;
 			CloseCommand = new DelegateCommand(ClosePage);
+
+			Name = "11 Boris Bidjan Saberi New York";
+			Address = "455 Greenwish Street, New York, United States";
+			ImageUri = "launchscreen";
+			Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+				"In diam est, tincidunt et lacus quis, luctus pellentesque dolor. Curabitur " +
+				"porttitor feugiat purus. Morbi efficitur vestibulum purus, a posuere magna. " +
+				"Sed et egestas libero. Morbi aliquet quam sit amet diam tempor, ut aliquet " +
+				"lectus pretium. Ut massa justo, dictum eu ipsum pharetra, sollicitudin suscipit " +
+				"metus. Maecenas in ullamcorper eros, ac ultricies dui. Proin quis nisi lacus. Cras " +
+				"porttitor neque nec aliquet laoreet.";
 		}
+
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string ImageUri { get; set; }
+		public string Description { get; set; }
 
 		public DelegateCommand CloseCommand { get; set; }
 
-		void ClosePage()
+		async void ClosePage()
 		{
-			_navigationService.GoBackAsync();
+			await _navigationService.GoBackAsync();
 		}
 
 		private INavigationService _navigationService;
