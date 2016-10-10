c83a9899422cf3a6f77aacf111cb3f1331e8dc21
William Bello <willjobs94@gmail.com>
2016-10-10 18:53:46 -0400

    Add DecimalValueConverter

---
+++ Farfetch/Farfetch/Converters/DecimalValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Farfetch
+{
+	public class DecimalValueConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is decimal)) return value;
+			return "$" + value;
+
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
